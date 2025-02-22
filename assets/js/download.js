fb28329fa2e4247699b8ea5a5e96d0e5ae784889
Black Whale <schwarzerwal@outlook.com>
2025-02-22 13:00:00 +0900

    Add Release 9.0
    Signed-off-by: Black Whale <schwarzerwal@outlook.com>

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,10 +1,10 @@
 const table = document.getElementById('download-list');
 
-const data = fetch("assets/json/download.json")
+const data = fetch("/assets/json/download.json")
 	.then(response => response.json())
 	.then(data => {
 		for (let j = 0; j < data.downloads.length; j++) {
-			const tbody = document.getElementById('tbody')
+			const tbody = document.getElementById('tbody');
 			const row = tbody.insertRow();
 
 			for (let i = 0; i < 6; i++) {
@@ -12,12 +12,29 @@ const data = fetch("assets/json/download.json")
 				let content = 0;
 
 				switch (i) {
-					case 0: content = data.downloads[j].build;        break;
-					case 1: content = data.downloads[j].basedVersion; break;
-					case 2: content = data.downloads[j].version;      break;
-					case 3: content = data.downloads[j].download_win; break;
-					case 4: content = data.downloads[j].download_mac; break;
-					case 5: content = data.downloads[j].download_lin; break;
+					case 0:
+						content = data.downloads[j].build;
+						break;
+
+					case 1:
+						content = data.downloads[j].basedVersion;
+						break;
+
+					case 2:
+						content = data.downloads[j].version;
+						break;
+
+					case 3:
+						content = data.downloads[j].download_win;
+						break;
+
+					case 4:
+						content = data.downloads[j].download_mac;
+						break;
+
+					case 5:
+						content = data.downloads[j].download_lin;
+						break;
 				}
 
 				if (i <= 2) {
@@ -25,22 +42,11 @@ const data = fetch("assets/json/download.json")
 				} else {
 					if (content == 0) continue;
 
-					if (data.downloads[j].unpackaged) {
-						cell.insertAdjacentHTML('afterbegin',
-							`<div class="tooltip">
-								<a href="` + content + `">
-									<div class="material-icons md-light">download</div>
-									<div class="tooltip-text">패키징되지 않은 버전이 다운로드됩니다.</div>
-								</a>
-							</div>`
-						);
-					} else {
-						cell.insertAdjacentHTML('afterbegin',
-							`<a href="` + content + `">
-								<div class="material-icons md-light">download</div>
-							</a>`
-						);
-					}
+					cell.insertAdjacentHTML('afterbegin',
+						`<a href="` + content + `">
+							<div class="material-icons md-light">download</div>
+						</a>`
+					);
 				}
 			}
 		}
